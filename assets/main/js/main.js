6237650a2e0a93188b21dd28163e1a9e0fb12631
Hamid Axtamov <hamidaxtamov1@gmail.com>
2023-12-30 09:30:08 +0500

    search front add

---
+++ assets/main/js/main.js
@@ -0,0 +1,23 @@
+let counter = 0;
+
+function navbarBurgerDeploy() {
+    let navBurger = document.getElementById('navBurger');
+    let customOffCanvas = document.getElementById('customOffCanvas');
+    let wrapper = document.getElementById('customOffCanvasWrapper');
+    let x_body = document.getElementsByTagName('body')[0];
+    let cart = document.getElementById('navCart')
+
+    if (counter % 2) {
+        customOffCanvas.style.width = 'min(450px, 100%)';
+        wrapper.style.visibility = 'visible';
+        wrapper.style.opacity = '1';
+        x_body.style.overflow = 'hidden';
+    } else {
+        customOffCanvas.style.width = '0px';
+        wrapper.style.visibility = 'hidden'
+        wrapper.style.opacity = '0';
+        x_body.style.overflow = 'auto';
+
+    }
+    counter += 1
+}
